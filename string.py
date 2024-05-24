6615411c7e5411939e8567226c3e6c60f556c202
Muhammad Sufyan <shakirali9749@gmail>
2024-05-24 05:03:22 -0700

    add the new string file

--- string.py
+++ string.py
@@ -68,12 +68,39 @@
 # print(code(list1,list2))
 
 
-def steairs(steps):
-	if steps % 2 == 0:
-		return int(steps/2)
-	total_steps = int((steps - 1)/2)
-	return f"steps:{total_steps} , and 1 steair"
+# def steairs(steps):
+# 	if steps % 2 == 0:
+# 		return int(steps/2)
+# 	total_steps = int((steps - 1)/2)
+# 	return f"steps:{total_steps} , and 1 steair"
 
-print(steairs(20))
+# print(steairs(20))
+
+
+
+# def devidearr(arr):
+# 	arr1=[]
+# 	arr2=[]
+# 	for i in range(len(arr)):
+# 		if i % 2 == 0:
+# 			arr1.append(arr[i])
+
+# 		else:
+# 			arr2.append(arr[i])
+# 	return f" new lists are { arr1} and {arr2}"
+# print(devidearr([1,3,5,4,6,57,5,"pakistan","source"]))
+
+
+
+
+def minimunvalue(arr):
+	first=arr[0]
+	
+	for i in range(len(arr)):
+		if arr[i]<first:
+			first=arr[i]
+	return first
+print(minimunvalue([33,66,33,4,6,5]))
+
 
 
